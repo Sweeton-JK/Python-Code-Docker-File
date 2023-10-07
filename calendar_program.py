41a63b2405822d032a23bc74bd9fe9006d923acd
Jeyanth Kallis Sweeton <sweeton.2001@outlook.com>
2023-10-07 14:16:06 +0530

    Completed

--- calendar_program.py
+++ calendar_program.py
@@ -1,7 +1,12 @@
 import calendar
+import sys
 
-year = int(input("Enter year (e.g., 2023): "))
-month = int(input("Enter month (1-12): "))
+if len(sys.argv) != 3:
+    print("Usage: python calendar_program.py <year> <month>")
+    sys.exit(1)
+
+year = int(sys.argv[1])
+month = int(sys.argv[2])
 
 if 1 <= month <= 12:
     cal = calendar.month(year, month)
